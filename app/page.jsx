5ce4a8a0b3fd0ee349f9a8ed61ee5d43b6566999
AAVRTI\deepak <deepak@aavrti.com>
2025-06-17 18:19:06 +0530

    bg gradient yayyy

--- app/page.jsx
+++ app/page.jsx
@@ -4,12 +4,12 @@ export default function Home() {
       {/* <Navbar />
       <HeroSection /> */}
       <div
-        style={{ backgroundSize: "500%" }}
-        className="absolute w-full h-full animate-background bg-gradient-to-l from-blue-500 via-pink-300 to-blue-500 "
+        style={{ backgroundSize: "200%" }}
+        className="absolute w-full h-full animate-background animated-bg-gradient bg-gradient-to-l from-blue-500 via-pink-300 to-blue-500 "
       />
-      <div className="tile grid grid-cols-10 grid-rows-10 w-full h-full gap-1">
+      <div className="grid grid-cols-10 grid-rows-10 w-full h-full">
         {Array.from({ length: 110 }).map((_, index) => (
-          <div key={index} className="w-full h-full"></div>
+          <div key={index} className="tile w-full h-full outline-tile"></div>
         ))}
       </div>
     </div>
